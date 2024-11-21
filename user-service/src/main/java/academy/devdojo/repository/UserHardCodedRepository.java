4f23fc5a203ddadf7deb78c6634aeb8b9983e717
Gustavo <gustavo_linhaus@hotmail.com>
2024-11-21 07:44:05 -0300

    video55: exercise user-service pt 02

--- user-service/src/main/java/academy/devdojo/repository/UserHardCodedRepository.java
+++ user-service/src/main/java/academy/devdojo/repository/UserHardCodedRepository.java
@@ -16,10 +16,12 @@ public class UserHardCodedRepository {
     public List<User> findAll() {
         return userData.getUsers();
     }
+
     public Optional<User> findById(Long id) {
         return userData.getUsers().stream().filter(user -> user.getId().equals(id)).findFirst();
     }
-    public List<User> findByName(String firstName) {
+
+    public List<User> findByFirstName(String firstName) {
         return userData.getUsers().stream().filter(user -> user.getFirstName().equalsIgnoreCase(firstName)).toList();
     }
 }
